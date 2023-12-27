fee223876d2d89aa5032c05bf19881f5e1517ee8
jiwan kim <jiwan.kim@kaonbroadband.com>
2023-12-27 11:16:40 +0900

    update page

--- src/views/projectcard.vue
+++ src/views/projectcard.vue
@@ -1,6 +1,6 @@
 <template>
 
-<el-card class="box-card-pjc">
+<div>
     <div style="text-align:center">
         <Typography v-if="true">
             <Title :level="3" class="title-left-padding">
@@ -12,7 +12,7 @@
     <div>
         <el-row class="responsive-row-pjc">
             <el-col :span="24"  @click="router_go_to_name('fear-and-greed')">
-                <el-card :shadow="'hover'" style="margin:20px">
+                <el-card :shadow="'hover'" class="card-margin">
                     <template #header>
                         <div>
                             <span>{{ "* 암호화폐 투심 지수" }}</span>
@@ -24,7 +24,7 @@
                 </el-card>
             </el-col>
             <el-col :span="24" @click="router_go_to_name('soccer')"> 
-                <el-card :shadow="'hover'" style="margin:20px">
+                <el-card :shadow="'hover'" class="card-margin">
                     <template #header>
                         <div>
                             <span>{{ "* 프리미어리그 순위/정보/일정" }}</span>
@@ -36,10 +36,10 @@
                 </el-card>
             </el-col>
             <el-col :span="24" @click="router_go_to_name('growing')"> 
-                <el-card :shadow="'hover'" style="margin:20px">
+                <el-card :shadow="'hover'" class="card-margin">
                     <template #header>
                         <div>
-                            <span>{{ "[Clone coding] 버섯커 키우기(by Joy-games)의 사전예약 페이지" }}</span>
+                            <span>{{ "* 버섯커 키우기 사전예약 (Clone by joy-games)" }}</span>
                         </div>
                     </template>
                     <div class="img-container ">
@@ -48,15 +48,14 @@
                 </el-card>
             </el-col>
             <el-col :span="24" @click="router_go_to_name('pok')"> 
-                <el-card :shadow="'hover'" style="margin:20px">
+                <el-card :shadow="'hover'" class="card-margin">
                     <template #header>
                         <div>
-                            <span>{{ "Pokemon (NOW IN PROGRESS)" }}</span>
+                            <span>{{ "* 포켓몬 도감 1세대" }}</span>
                         </div>
                     </template>
-                    <div style="text-align:center;">
-                        {{"IMAGE WILL BE HERE"}}<br>
-                        {{"NOW PROJECT IS IN PROGRESS"}}
+                    <div class="img-container ">
+                        <Image :src="this.imgList[3]"/>
                     </div>
                     <!-- <img
                         src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
@@ -78,18 +77,20 @@
             </el-col> -->
         </el-row>
     </div>
-</el-card>
-
+</div>
 </template>
 <script>
 import fgimg from '@/assets/feargreedimg.png'
 import fbimg from '@/assets/football.png'
 import cljyimg from '@/assets/clonejoygame.png'
+import pkming from '@/assets/pokemonlogo.png'
+
     export default {
         components: {
             fgimg,
             fbimg,
-            cljyimg
+            cljyimg,
+            pkming
         },
         data () {
             return {
@@ -97,7 +98,8 @@ import cljyimg from '@/assets/clonejoygame.png'
                 imgList : [
                     fgimg,
                     fbimg,
-                    cljyimg
+                    cljyimg,
+                    pkming
                 ]
             }
         },
@@ -120,9 +122,12 @@ import cljyimg from '@/assets/clonejoygame.png'
         align-items: center;
 
     }
+    .card-margin {
+        margin: 20px;
+    }
     @media screen and (max-width: 600px) {
         .box-card-pjc {
-            margin: 10px;
+            margin: 0px;
             width: auto;
         }
         .el-row .el-col {
@@ -136,6 +141,9 @@ import cljyimg from '@/assets/clonejoygame.png'
         }
         .el-col-12 {
             max-width: none;
+        }
+        .card-margin {
+            margin: 5px;
         }
     }
     /* .image {
